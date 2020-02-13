da2e577d9ea72d99482ecb8ff0e714370fd771f1
guy <reborn14412@hotmail.com>
2020-02-13 16:15:19 +0700

    no message

---
+++ CRUDCore/Models/db/CustomerCustomerDemo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CRUDCore.Models.db
+{
+    public partial class CustomerCustomerDemo
+    {
+        public string CustomerId { get; set; }
+        public string CustomerTypeId { get; set; }
+
+        public Customers Customer { get; set; }
+        public CustomerDemographics CustomerType { get; set; }
+    }
+}
---
+++ CRUDCore/Startup.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore; //call framework
+using CRUDCore.Models.db; //call my project
+
+namespace CRUDCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            //Connect n call class Northwindcontext
+            var connection = @"Server=.\SQLExpress04;Database=Northwind;Trusted_COnnection=True;ConnectRetryCount=0";
+            services.AddDbContext<NorthwindContext>(options => options.UseSqlServer(connection));
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Products}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
